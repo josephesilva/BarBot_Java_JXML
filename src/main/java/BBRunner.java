2e3cf7594f9c62285bc61eae5dcc216da3e57e37
Joseph Silva <josephesilva12@gmail.com>
2016-02-01 22:03:01 -0500

    created repo

---
+++ src/main/java/BBRunner.java
@@ -0,0 +1,19 @@
+
+public class BBRunner {
+	
+	public BBRunner(){
+		
+	}
+	
+	public int run(){
+		int exitCode = 0;
+		return exitCode;
+	}
+	
+	
+	public static void main(String[] args) {
+		BBRunner runner = new BBRunner();
+		runner.run();
+	}
+
+}
